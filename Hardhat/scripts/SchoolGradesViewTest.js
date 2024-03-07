53a8e803897fbbb445a25ead0b6680021e1fa345
Jesus <jesusrosas.dm@gmail.com>
2024-03-06 18:23:18 -0600

    Update School Grades

--- Hardhat/scripts/SchoolGradesViewTest.js
+++ Hardhat/scripts/SchoolGradesViewTest.js
@@ -15,7 +15,11 @@ async function main() {
     console.log("grades :", grades);
 }
 
-//0x27193d65DE70BF07AA1Ce71Cb316cb4c3A485bca  student1
+//0x27193d65DE70BF07AA1Ce71Cb316cb4c3A485bca  teacher
+//0x8Ed6e5a15d3DF9eB8f767B6557B0c815bE04d6cD student2
+//0x46b05e5Cc3091553297BfDC143FD035bC04c5de4 principal
+//0xEDee67926Ff5cB756D50C5A6bECAe8945279003c student1
+// 0x9AecC2114B79b3d92e284D62fb1bBB06735110b7 student3
 
 main().catch((error) => {
     console.error(error);
